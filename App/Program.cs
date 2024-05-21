2dec2f8c40202ec5712d0fac604e28e2fd3ef853
FabioPenedo <fabiopenedo21@gmail.com>
2024-05-21 14:50:19 -0300

    expressao lambda inline

--- App/Program.cs
+++ App/Program.cs
@@ -11,17 +11,12 @@ namespace App // Note: actual namespace depends on the project name.
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            list.Sort(CompareProducts);
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach (Product item in list)
             {
                 Console.WriteLine(item);
             }
-        }
-
-        static int CompareProducts(Product p1,  Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
