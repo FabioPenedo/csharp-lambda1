c08754b5d7f6c65da4618fb4a473bb7e1f4c4919
FabioPenedo <fabiopenedo21@gmail.com>
2024-05-21 14:44:14 -0300

    primeiro commit, Icomprable implementacao

---
+++ App/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace App.Entities
+{
+    internal class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Product? obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: argument is not an Product");
+            }
+   
+            return Name.ToUpper().CompareTo(obj.Name.ToUpper());
+        }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,22 @@
+﻿using App.Entities;
+
+namespace App // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = [];
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort();
+
+            foreach (Product item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
